0211706b8e9efe8b31c7ea4794ca2b90a15d74a5
Ademir Pedro <Maninhoo111@gmail.com>
2026-03-18 18:52:40 -0300

    ..

---
+++ script.js
@@ -0,0 +1,55 @@
+   const numeroAlvo = Math.floor(Math.random() * 100) + 1;
+
+        let tentativasRestantes = 10;
+
+        function vefificarpalpite(){
+            const palpite = parseInt(document.getElementById('campoPalpite').value);
+            const mensagem = document.getElementById('mensagem');
+            const tentativasSpan = document.getElementById('tentativas');
+            const botaoRecarregar = document.getElementById('botaoRecarregar');
+
+        function LimparClassesMensagem(){
+            mensagem.classList.remove("acertou", "maior", "menor", "perdeu");
+        }
+    
+
+            if(isNaN(palpite) || palpite <1 || palpite > 100){
+                mensagem.textContent = 'Por favor, insira um numero valido entre 1 e 100.';
+            }else{
+                tentativasRestantes--;
+                tentativasSpan.textContent = tentativasRestantes;
+
+                if(palpite === numeroAlvo){
+                    mensagem.textContent = `Parabens! Voce acertou o numero ${numeroAlvo}. `;
+                    botaoRecarregar.style.display = 'block';
+                    mensagem.style.color = 'green';
+                    desabilitarEntrada();
+                    
+
+                }  else if(palpite < numeroAlvo){
+                    mensagem.textContent = 'Tente um numero maior.';
+                    LimparClassesMensagem();
+                    mensagem.classList.add('maior');
+                }
+                else if(palpite > numeroAlvo){
+                    mensagem.textContent = 'tente numero menor. ';
+                    LimparClassesMensagem();
+                    mensagem.classList.add('menor');
+
+                }
+                if(tentativasRestantes === 0){
+                    mensagem.textContent = `Acabaram suas tentativas. O numero correto era ${numeroAlvo}. `;
+                    LimparClassesMensagem();
+                    mensagem.classList.add('perdeu');
+                    desabilitarEntrada();
+                    botaoRecarregar.style.display = 'block';
+                }
+            }
+        }
+        function desabilitarEntrada(){
+            document.getElementById('campoPalpite').disabled = true;
+            document.getElementById('botaoEnviar').disabled = true;
+        }
+        function recarregarPagina(){
+            location.reload();
+        }
